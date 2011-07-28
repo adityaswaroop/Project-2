47bfe2bf7107989515e9658ab9cd396cc1389e8a
ranjeet <ranjeetkumarkanaily@gmail.com>
2011-07-28 16:19:13 +0530

    ranjeet updates

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ActiveRecord::Base
+  validates_presence_of :group_name, :company_name, :responsible_person, :company_type, :address, :city, :state, :country , :message => "Field should not be empty! "
+  validates_uniqueness_of :company_name
 end
