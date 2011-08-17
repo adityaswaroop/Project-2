d88e38d21b04b4af101cf2920a2419f2d30a5d30
Admin <rsl@guruprasad.(none)>
2011-08-17 11:02:49 +0530

    Salary Forms and Menu Addedd

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Project2::Application.routes.draw do
   resources :companies
   resources :employees
   resources :timemanagements
+  resources :salaries
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
