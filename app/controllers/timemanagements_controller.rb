eb475a50de2383fa11ea69fec9c97154265e5db6
rohini <rohini.rao.r@gmail.com>
2011-08-18 14:44:06 +0530

    shift master screen added

--- app/controllers/timemanagements_controller.rb
+++ app/controllers/timemanagements_controller.rb
@@ -1,5 +1,10 @@
 class TimemanagementsController < ApplicationController
   # To change this template use File | Settings | File Templates.
 
+def show
+  respond_to do |format|
+    format.html #showhtml.haml
+  end
+end
 
 end
